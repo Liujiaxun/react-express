97e335e633f85b3ac0f78fc978128f2cb0dc1fe2
liujiaxun <156625306@qq.com>
2018-12-21 11:55:31 +0800

    新增axios

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,65 @@
 import React,{Component} from 'react';
-import {Card} from 'antd';
+import { Form, Icon, Input, Button, Checkbox, } from 'antd';
+const FormItem = Form.Item;
 
 class Login extends Component {
     state = {
         userName:''
     }
-
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+          }
+        });
+      }
     render(){
+        const { getFieldDecorator } = this.props.form;
         return (
             <div className="login">
-                <div className="loginForm">
-                <Card
-                    title="Card title"
-                    extra={<a href="#">More</a>}
-                    style={{ width: 300 }}
-                >
-                    <p>Card content</p>
-                    <p>Card content</p>
-                    <p>Card content</p>
-                </Card>
+                <div className="whiteBack">
+                    <div className="loginForm">
+                        <div className="login-logo">
+                            Admin
+                        </div>
+                        
+                        <Form onSubmit={this.handleSubmit} className="login-form">
+                            <FormItem>
+                            {getFieldDecorator('userName', {
+                                rules: [{ required: true, message: 'Please input your username!' }],
+                            })(
+                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                            )}
+                            </FormItem>
+                            <FormItem>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: 'Please input your Password!' }],
+                            })(
+                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+                            )}
+                            </FormItem>
+                            <FormItem>
+                            {getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true,
+                            })(
+                                <Checkbox>Remember me</Checkbox>
+                            )}
+                            <a className="login-form-forgot" href="/">Forgot password</a>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                Log in
+                            </Button>
+                            Or <a href="/">register now!</a>
+                            </FormItem>
+                        </Form>
+                        
+                    </div>
+                    <div className="back"></div>
                 </div>
             </div>
         );
     }
 }
 
-export default Login;
+export default Form.create()(Login);
